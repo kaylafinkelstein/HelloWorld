ddbc09213b64ff03ea3f7def053b56828d864fba
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-04-12 13:01:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kayla! You can do this!);
+        }
+    }
+}
